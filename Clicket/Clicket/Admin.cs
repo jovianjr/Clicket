3d69be09939bff24506c058fce0172c6bca09b8f
jovian <alphonsusjovian@gmail.com>
2022-09-12 15:25:33 +0700

    feat(class): add User and Admin class

---
+++ Clicket/Clicket/Admin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum TicketStatus
+{
+    Verified,
+    NotVerified,
+    Progres
+}
+
+namespace Clicket
+{
+    class Admin
+    {
+        public Boolean MakeEvent(string title, string description, DateTime startDate, DateTime endDate, int price, int quota, string imgURL)
+        {
+            return false;
+        }
+
+        public Boolean UpdateEvent(string title, string description, DateTime startDate, DateTime endDate, int price, int quota, string imgURL)
+        {
+            return false;
+        }
+
+        public Boolean DeleteEvent(int eventID)
+        {
+            return false;
+        }
+
+        public Boolean MakeMovie(string title, string description, DateTime date, int durationHour, int durationMin, int price, int quota, string imgURL, string[] genre, string ageRate)
+        {
+            return false;
+        }
+
+        public Boolean UpdateMovie(string title, string description, DateTime date, int durationHour, int durationMin, int price, int quota, string imgURL, string[] genre, string ageRate)
+        {
+            return false;
+        }
+
+        public Boolean DeleteMovie(int movieID)
+        {
+            return false;
+        }
+
+        public Boolean ConfirmEventTicket(int userID, int eventID, TicketStatus status)
+        {
+            return false;
+        }
+
+        public Boolean ConfirmMovieTicket(int userID, int eventID, TicketStatus status)
+        {
+            return false;
+        }
+    }
+}
