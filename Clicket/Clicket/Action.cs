829a93bc041802adfb3684900b7b879e21b507e9
jovian <alphonsusjovian@gmail.com>
2022-11-14 10:11:14 +0700

    feat: add db insert

---
+++ Clicket/Clicket/Action.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicket
+{
+    internal class Action
+    {
+        DB db = new DB();
+        public void add(Movie movieItem)
+        {
+            db.insert(movieItem);
+        }
+        public void add(Event eventItem)
+        {
+            db.insert(eventItem);
+        }
+
+
+    }
+}
---
+++ Clicket/Clicket/DB.cs
@@ -0,0 +1,67 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicket
+{
+    internal class DB
+    {
+        public NpgsqlConnection conn;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private string connstring = "Host=localhost;Port=5432;Username=postgres;Password=123;Database=clicket";
+
+
+        public DB()
+        {
+            conn = new NpgsqlConnection(connstring);
+        }
+        public void insert(Movie movieItem)
+        {
+            conn.Open();
+            string sql = @"select * from insert_movie(:_title, :_description, :_date, :_durationHour, :_durationMin, :_price, :_quota, :_imgUrl, :_genre, :_ageRate)";
+            cmd = new NpgsqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("_title", movieItem.Title);
+            cmd.Parameters.AddWithValue("_description", movieItem.Description);
+            cmd.Parameters.AddWithValue("_date", movieItem.Date);
+            cmd.Parameters.AddWithValue("_durationHour", movieItem.DurationHour);
+            cmd.Parameters.AddWithValue("_durationMin", movieItem.DurationMin);
+            cmd.Parameters.AddWithValue("_price", movieItem.Price);
+            cmd.Parameters.AddWithValue("_quota", movieItem.Quota);
+            cmd.Parameters.AddWithValue("_imgUrl", movieItem.ImgURL);
+            cmd.Parameters.AddWithValue("_genre", movieItem.Genre);
+            cmd.Parameters.AddWithValue("_ageRate", movieItem.ageRate);
+
+            if ((int)cmd.ExecuteScalar() == 1)
+            {
+                MessageBox.Show("Movie berhasil ditambahkan", "Well Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            conn.Close();
+        }
+        public void insert(Event eventItem)
+        {
+            conn.Open();
+            string sql = @"select * from insert_event(:_title, :_description, :_startDate, :_endDate, :_price, :_quota, :_imgUrl)";
+            cmd = new NpgsqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("_title", eventItem.Title);
+            cmd.Parameters.AddWithValue("_description", eventItem.Description);
+            cmd.Parameters.AddWithValue("_startDate", eventItem.StartDate);
+            cmd.Parameters.AddWithValue("_endDate", eventItem.EndDate);
+            cmd.Parameters.AddWithValue("_price", eventItem.Price);
+            cmd.Parameters.AddWithValue("_quota", eventItem.Quota);
+            cmd.Parameters.AddWithValue("_imgUrl", eventItem.ImgURL);
+
+            if ((int)cmd.ExecuteScalar() == 1)
+            {
+                MessageBox.Show("Event berhasil ditambahkan", "Well Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            conn.Close();
+        }
+    }
+}
