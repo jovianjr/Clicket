8437a623da4d450175123ef352a5c6efa018370b
Iqbalrez <iqbalrr02@gmail.com>
2022-11-19 21:15:21 +0700

    edit MovieItem

--- Clicket/Clicket/MovieItem.Designer.cs
+++ Clicket/Clicket/MovieItem.Designer.cs
@@ -32,12 +32,12 @@
             this.lbTitle = new System.Windows.Forms.Label();
             this.pb_poster = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblLocation = new System.Windows.Forms.Label();
-            this.lblDate = new System.Windows.Forms.Label();
-            this.lblGenre = new System.Windows.Forms.Label();
+            this.lbLocation = new System.Windows.Forms.Label();
+            this.lbDate = new System.Windows.Forms.Label();
+            this.lbGenre = new System.Windows.Forms.Label();
             this.btn_Details = new System.Windows.Forms.Button();
-            this.lblPrice = new System.Windows.Forms.Label();
-            this.lblDur = new System.Windows.Forms.Label();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.lbDur = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pb_poster)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -81,48 +81,48 @@
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.lblLocation);
-            this.panel1.Controls.Add(this.lblDate);
-            this.panel1.Controls.Add(this.lblGenre);
+            this.panel1.Controls.Add(this.lbLocation);
+            this.panel1.Controls.Add(this.lbDate);
+            this.panel1.Controls.Add(this.lbGenre);
             this.panel1.Controls.Add(this.btn_Details);
-            this.panel1.Controls.Add(this.lblPrice);
+            this.panel1.Controls.Add(this.lbPrice);
             this.panel1.Location = new System.Drawing.Point(5, 5);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(365, 706);
             this.panel1.TabIndex = 20;
             // 
-            // lblLocation
+            // lbLocation
             // 
-            this.lblLocation.AutoSize = true;
-            this.lblLocation.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblLocation.Location = new System.Drawing.Point(18, 599);
-            this.lblLocation.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblLocation.Name = "lblLocation";
-            this.lblLocation.Size = new System.Drawing.Size(98, 26);
-            this.lblLocation.TabIndex = 15;
-            this.lblLocation.Text = "Location";
+            this.lbLocation.AutoSize = true;
+            this.lbLocation.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbLocation.Location = new System.Drawing.Point(18, 599);
+            this.lbLocation.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbLocation.Name = "lbLocation";
+            this.lbLocation.Size = new System.Drawing.Size(98, 26);
+            this.lbLocation.TabIndex = 15;
+            this.lbLocation.Text = "Location";
             // 
-            // lblDate
+            // lbDate
             // 
-            this.lblDate.AutoSize = true;
-            this.lblDate.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDate.Location = new System.Drawing.Point(18, 573);
-            this.lblDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(58, 26);
-            this.lblDate.TabIndex = 14;
-            this.lblDate.Text = "Date";
+            this.lbDate.AutoSize = true;
+            this.lbDate.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbDate.Location = new System.Drawing.Point(18, 573);
+            this.lbDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbDate.Name = "lbDate";
+            this.lbDate.Size = new System.Drawing.Size(58, 26);
+            this.lbDate.TabIndex = 14;
+            this.lbDate.Text = "Date";
             // 
-            // lblGenre
+            // lbGenre
             // 
-            this.lblGenre.AutoSize = true;
-            this.lblGenre.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblGenre.Location = new System.Drawing.Point(18, 547);
-            this.lblGenre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblGenre.Name = "lblGenre";
-            this.lblGenre.Size = new System.Drawing.Size(71, 26);
-            this.lblGenre.TabIndex = 13;
-            this.lblGenre.Text = "Genre";
+            this.lbGenre.AutoSize = true;
+            this.lbGenre.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbGenre.Location = new System.Drawing.Point(18, 547);
+            this.lbGenre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbGenre.Name = "lbGenre";
+            this.lbGenre.Size = new System.Drawing.Size(71, 26);
+            this.lbGenre.TabIndex = 13;
+            this.lbGenre.Text = "Genre";
             // 
             // btn_Details
             // 
@@ -138,38 +138,38 @@
             this.btn_Details.UseVisualStyleBackColor = false;
             this.btn_Details.Click += new System.EventHandler(this.btn_Details_Click);
             // 
-            // lblPrice
+            // lbPrice
             // 
-            this.lblPrice.AutoSize = true;
-            this.lblPrice.BackColor = System.Drawing.Color.White;
-            this.lblPrice.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblPrice.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.lblPrice.Location = new System.Drawing.Point(17, 653);
-            this.lblPrice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(112, 26);
-            this.lblPrice.TabIndex = 10;
-            this.lblPrice.Text = "Rp 25.000";
-            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.BackColor = System.Drawing.Color.White;
+            this.lbPrice.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbPrice.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbPrice.Location = new System.Drawing.Point(17, 653);
+            this.lbPrice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(112, 26);
+            this.lbPrice.TabIndex = 10;
+            this.lbPrice.Text = "Rp 25.000";
+            this.lbPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // lblDur
+            // lbDur
             // 
-            this.lblDur.AutoSize = true;
-            this.lblDur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(195)))), ((int)(((byte)(0)))));
-            this.lblDur.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDur.Location = new System.Drawing.Point(276, 479);
-            this.lblDur.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblDur.Name = "lblDur";
-            this.lblDur.Size = new System.Drawing.Size(64, 20);
-            this.lblDur.TabIndex = 22;
-            this.lblDur.Text = "1h 40m";
+            this.lbDur.AutoSize = true;
+            this.lbDur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(195)))), ((int)(((byte)(0)))));
+            this.lbDur.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbDur.Location = new System.Drawing.Point(276, 479);
+            this.lbDur.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbDur.Name = "lbDur";
+            this.lbDur.Size = new System.Drawing.Size(64, 20);
+            this.lbDur.TabIndex = 22;
+            this.lbDur.Text = "1h 40m";
             // 
             // MovieItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.lblDur);
+            this.Controls.Add(this.lbDur);
             this.Controls.Add(this.lbAgeRate);
             this.Controls.Add(this.lbTitle);
             this.Controls.Add(this.pb_poster);
@@ -190,11 +190,11 @@
         private Label lbTitle;
         private PictureBox pb_poster;
         private Panel panel1;
-        private Label lblLocation;
-        private Label lblDate;
-        private Label lblGenre;
+        private Label lbLocation;
+        private Label lbDate;
+        private Label lbGenre;
         private Button btn_Details;
-        private Label lblPrice;
-        private Label lblDur;
+        private Label lbPrice;
+        private Label lbDur;
     }
 }
--- Clicket/Clicket/MovieItem.cs
+++ Clicket/Clicket/MovieItem.cs
@@ -22,20 +22,42 @@ namespace Clicket
 
         private string _movieTitle;
         private string _ageRate;
+        private string _duration;
+        private string _genre;
+        private string _date;
+        private string _location;
         private Image _poster;
 
-        [Category("CustomProps")]
         public string title
         {
             get { return _movieTitle; }
             set { _movieTitle = value; lbTitle.Text = value; }
         }
 
-        [Category("CustomProps")]
         public string ageRate
         {
             get { return _ageRate; }
             set { _ageRate = value; lbAgeRate.Text = value; }
+        }
+        public string genre
+        {
+            get { return _genre; }
+            set { _genre = value; lbGenre.Text = value; }
+        }
+        public string date
+        {
+            get { return _date; }
+            set { _date = value; lbDate.Text = value; }
+        }
+        public string location
+        {
+            get { return _location; }
+            set { _location = value; lbLocation.Text = value; }
+        }
+        public string duration
+        {
+            get { return _duration; }
+            set { _duration = value; lbDur.Text = /*_durHour + "*/ "h " /* + _durMinute*/ + "m"; }
         }
 
         private void btn_Details_Click(object sender, EventArgs e)
