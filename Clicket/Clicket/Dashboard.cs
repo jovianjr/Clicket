8e5bcf86fa0393df472088bd0cf40428bc7a34a0
Iqbalrez <iqbalrr02@gmail.com>
2022-11-20 18:58:33 +0700

    add History section

--- Clicket/Clicket/Dashboard.cs
+++ Clicket/Clicket/Dashboard.cs
@@ -37,7 +37,7 @@ namespace Clicket
             }
 
             EventItem[] eventItems = new EventItem[20];
-            for (int i = 0; i < movieItems.Length; i++)
+            for (int i = 0; i < eventItems.Length; i++)
             {
                 eventItems[i] = new EventItem();
                 eventItems[i].title = "Event " + i.ToString();
@@ -49,12 +49,28 @@ namespace Clicket
                 //else
                 flp_event.Controls.Add(eventItems[i]);
             }
+
+            History[] histories = new History[20];
+            for (int i = 0; i < histories.Length; i++)
+            {
+                histories[i] = new History();
+                histories[i].title = i.ToString();
+                histories[i].date = i.ToString();
+                histories[i].location = i.ToString();
+                histories[i].price = i.ToString();
+                histories[i].qty = i.ToString();
+                histories[i].total = i.ToString();
+                histories[i].status = i.ToString();
+
+                flp_history.Controls.Add(histories[i]);
+            }
         }
 
         private void Dashboard_Load(object sender, EventArgs e)
         {
             populateItems();
             flp_event.Visible = false;
+            flp_history.Visible = false;
             btn_Movie.BackColor = Color.FromArgb(255, 195, 0);
             iconMovie.BackColor = Color.FromArgb(255, 195, 0);
         }
@@ -63,28 +79,47 @@ namespace Clicket
         {
             flp_movie.Visible = true;
             flp_event.Visible = false;
+            flp_history.Visible = false;
+
             lbTitle.Text = "#MOVIES";
             lbSubTitle.Text = "NOW SHOWING";
 
-            btn_Event.BackColor = Color.White;
-            iconEvent.BackColor = Color.White;
+            btn_Movie.BackColor = iconMovie.BackColor = Color.FromArgb(255, 195, 0);
 
-            btn_Movie.BackColor = Color.FromArgb(255, 195, 0);
-            iconMovie.BackColor = Color.FromArgb(255, 195, 0);
+            btn_Event.BackColor = iconEvent.BackColor = Color.White;
+
+            btnHistory.BackColor = iconHistory.BackColor = Color.White;
         }
 
         private void btn_Event_Click(object sender, EventArgs e)
         {
             flp_movie.Visible = false;
             flp_event.Visible = true;
+            flp_history.Visible = false;
+
             lbTitle.Text = "#EVENTS";
             lbSubTitle.Text = "WILL BE HELD";
 
-            btn_Movie.BackColor = Color.White;
-            iconMovie.BackColor = Color.White;
+            btn_Movie.BackColor = iconMovie.BackColor = Color.White;
 
-            btn_Event.BackColor = Color.FromArgb(255, 195, 0);
-            iconEvent.BackColor = Color.FromArgb(255, 195, 0);
+            btn_Event.BackColor = iconEvent.BackColor = Color.FromArgb(255, 195, 0);
+
+            btnHistory.BackColor = iconHistory.BackColor = Color.White;
+        }
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            flp_movie.Visible = false;
+            flp_event.Visible = false;
+            flp_history.Visible = true;
+
+            lbTitle.Text = "#HISTORY";
+            lbSubTitle.Text = "Transaction History List";
+
+            btn_Movie.BackColor = iconMovie.BackColor = Color.White;
+
+            btn_Event.BackColor = iconEvent.BackColor = Color.White;
+
+            btnHistory.BackColor = iconHistory.BackColor = Color.FromArgb(255, 195, 0);
         }
 
         private void iconEvent_Click(object sender, EventArgs e)
@@ -95,6 +130,11 @@ namespace Clicket
         private void iconMovie_Click(object sender, EventArgs e)
         {
             btn_Movie.PerformClick();
+        }
+
+        private void iconHistory_Click(object sender, EventArgs e)
+        {
+            btnHistory.PerformClick();
         }
     }
 }
--- Clicket/Clicket/Properties/Resources.Designer.cs
+++ Clicket/Clicket/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Clicket.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap history {
+            get {
+                object obj = ResourceManager.GetObject("history", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ikon_event {
             get {
                 object obj = ResourceManager.GetObject("ikon event", resourceCulture);
