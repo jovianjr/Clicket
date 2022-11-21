da296607acbe9961e9d3702b0b62d98fdd356da9
Iqbalrez <iqbalrr02@gmail.com>
2022-11-21 11:22:23 +0700

    fix Logo path

--- Clicket/Clicket/Properties/Resources.Designer.cs
+++ Clicket/Clicket/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Clicket.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Clicket_logo {
-            get {
-                object obj = ResourceManager.GetObject("Clicket logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap history {
             get {
                 object obj = ResourceManager.GetObject("history", resourceCulture);
